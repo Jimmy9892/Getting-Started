25ab00e15d147c75175ce31c76e78de254812b23
Jimmy <38862552+JxmmyBTW@users.noreply.github.com>
2018-05-13 13:17:36 -0400

    Add files via upload

---
+++ PluginTemplate.plugin.js
@@ -0,0 +1,9 @@
+//META{"name":"Plugin"}*//
+
+class ChannelSorter {
+		getName() { return "Plugin"; }
+		getShortName() { return "Plugin"; }
+		getDescription() { return "Plugin description."; }
+		getVersion() { return "1.0.0"; }
+		getAuthor() { return "YourName"; }
+	}
